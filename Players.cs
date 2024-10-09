045f8247dce65ecdea83cc69cc4b98a1fb38cb8f
Alanoud <Alanoud2022it@gmail.com>
2024-10-09 13:12:46 +0400

    Method to generate a random player name

--- Players.cs
+++ Players.cs
@@ -19,12 +19,21 @@ namespace FootballGame
             PlayerPosition=playerPosition;
             SkillesLevel=skillesLevel;
         }
+        //using array here because here same type and it is fixed size : 11 players
         private static Random random = new Random();
         private static string[] names = { "Tom", "Jerry", "Chris", "Mike", "John", "Alex", "Max", "Leo", "Sam", "Ryan", "Ethan" };
         private static string[] positions = { "Forward", "Midfielder", "Defender", "Goalkeeper" };
-        
+
+        // Method to generate a random player name
+        private string GenerateRandomName()
+        {
+            // Generate a random index from the names array
+            int randomIndex = random.Next(0, names.Length);
+            return names[randomIndex];
+        }
 
         
+
         public void DisplayInfo()
         {
             Console.WriteLine($"player name :{PlayerName},player position:{PlayerPosition},skill:{SkillesLevel}");

47b3c865d90e1dac2505d4b5e3ef4123985320fc
Alanoud <Alanoud2022it@gmail.com>
2024-10-09 12:57:41 +0400

    add some of the user input

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Welcome to the Soccer Game Simulator!");
+            Console.WriteLine("Enter name for Team 1:");
+            string teamName1 = Console.ReadLine();
+            Console.WriteLine("Enter name for Team 2:");
+            string teamName2 = Console.ReadLine();
         }
     }
 }

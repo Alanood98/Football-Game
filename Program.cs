f2f7a1ca0c718e4bac5ce0289a4e4f88e76d3037
Alanoud <Alanoud2022it@gmail.com>
2024-10-09 14:57:40 +0400

    updates in program file

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@
             string teamName1 = Console.ReadLine();
             Console.WriteLine("Enter name for Team 2:");
             string teamName2 = Console.ReadLine();
+            Team team1 = new Team(teamName1);
+            Team team2 = new Team(teamName2);
+            team1.GenerateTeamPlayers(11);
+            team2.GenerateTeamPlayers(11);
         }
     }
 }

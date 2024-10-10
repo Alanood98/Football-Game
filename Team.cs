d1cf6e762a501231d733c38eed66e5ac4a46acbb
Alanoud <Alanoud2022it@gmail.com>
2024-10-10 12:45:28 +0400

    last update

--- Team.cs
+++ Team.cs
@@ -1,49 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace FootballGame
 {
-    public class Team : IDisplayInfo
+    public class Team
     {
+        //Attributes:
         public string TeamName;
-        public Players players;
-        public Players[] Players;
-        static   int Score =0;
-        
-
-
-
-        public  Team(string teamName , int score) 
+        public Player[] PlayerList;
+        //Constructor
+        public Team(string teamName, Player[] PlayerList)
         {
-            TeamName = teamName;
-            Score = score;
-            Players = new Players[11];
-
-        }
-
-        public void GenerateTeamPlayers()
-        {
-            for (int i = 0; i < Players.Length; i++)
-            {
-                Players[i] = Players.GenerateRandomPlayer();
-            }
-        }
-
-        // Display team information
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Team Name: {TeamName}, Score: {Score}");
-            Console.WriteLine("Team Roster:");
-            foreach (var player in Players)
-            {
-                player.DisplayInfo();
-            }
+            this.TeamName = teamName;
+            this.PlayerList = PlayerList;
         }
     }
-
 }
-
